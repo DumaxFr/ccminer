733b2e31c22684f391d696614db3dfafae408f05
DumaxFr <35855637+DumaxFr@users.noreply.github.com>
2018-06-10 20:40:11 +0200

    damn keyboard

--- phi/cuda_phi2.h
+++ phi/cuda_phi2.h
@@ -1,7 +1,7 @@
 #include "x11/cuda_x11.h"
 #include "Algo512/cuda_b_cubehash512.h"
 #include "Algo512/cuda_b_echo512.h"
-#include "Algo512/scuda_b_jh512.h"
+#include "Algo512/cuda_b_jh512.h"
 #include "cuda_phi2_skein512.h"
 
 extern void cuda_phi2_lyra2_cpu_init(uint64_t *);
